38f1610034e9301f3365d649c40d3d5784728f9a
dsjramesh7 <rameshdussa07@gmail.com>
2024-07-17 13:11:03 +0530

    practice

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -4,6 +4,9 @@ import Navbar from "./components/Navbar";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
   const [todo, setTodo] = useState([]);
 
@@ -20,17 +23,24 @@ const App = () => {
     };
     setTodo(todo.unshift(newTask));
     localStorage.setItem("todos", JSON.stringify(todo));
-    alert("New Task Added");
+    // alert("New Task Added");
+    toast.success("New Task Added!!!");
     setTodo(JSON.parse(localStorage.getItem("todos")));
   };
 
-  const deleteTask = () => {};
+  const deleteTask = (id) => {
+    const filteredTodos = todo.filter((item) => item.id != id);
+    localStorage.setItem("todos", JSON.stringify(filteredTodos));
+    toast.success("Task Deleted Successfully!!!")
+    setTodo(filteredTodos);
+  };
 
   return (
     <>
       <Navbar />
       <TodoForm addTask={addTask} />
       <TodoList todo={todo} deleteTask={deleteTask} />
+      <ToastContainer position="top-right" theme="dark" />
     </>
   );
 };
