4933bc3f6ae02af323e2938d04a38cffa39a057d
dsjramesh7 <rameshdussa07@gmail.com>
2024-07-22 12:26:24 +0530

    practice

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Navbar";
+import { TbGridDots } from "react-icons/tb";
 
-const Home = () => {
+const Home = ({ show, setShow }) => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="page homePage">
+      <Navbar show={show} />
+      <TbGridDots
+        className="hamburger"
+        onClick={() => {
+          setShow(!show);
+        }}
+      />
+      <div className="banner">
+        <h1>Ramesh</h1>
+        <h1>D</h1>
+        <p>Web Developer</p>
+        <div className="btns">
+          <Link to="/resume">Resume</Link>
+          <Link to="/portfolio">Portfolio</Link>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
-const Navbar = ({show}) => {
+import React from "react";
+import {Link} from "react-router-dom";
+import {CiMobile3} from 'react-icons/ci'
+const Navbar = ({ show }) => {
   return (
-    <nav className={show ? 'nav mobile-nav': 'nav'}>Navbar</nav>
-  )
-}
+    <nav className={show ? "nav mobile-nav" : "nav"}>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/resume">Resume</Link>
+        <Link to="/portfolio">Portfolio</Link>
+      </ul>
+      <div className="phone">
+        <CiMobile3/>
+        <span>1234567890</span>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
